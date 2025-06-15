e58a0fbe81180ed650f511ea4f0b7c0a26a1277d
Ruben <174733720+ruben-23@users.noreply.github.com>
2025-06-15 19:32:15 +0300

    Schimbare pozitie camera

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const h = window.innerHeight;
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
-camera.position.z = 5;
+camera.position.z = 18;
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(w, h);
 renderer.setClearColor(0x667eea);
